49a467693ba26bda28a933267f34da64c293920e
manthannayak2 <bomanthan51@gmail.com>
2025-11-09 11:11:57 +0530

    Fixed host and port

--- app.py
+++ app.py
@@ -57,4 +57,6 @@ def serve_output(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
