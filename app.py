21e4ce2a13d55a613a97e45bd0cc73ad50655ab7
manthannayak2 <bomanthan51@gmail.com>
2025-11-14 17:31:31 +0530

    new

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def static_files(path):
     return send_from_directory(os.path.join(BASE_DIR, 'Frontend'), path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
